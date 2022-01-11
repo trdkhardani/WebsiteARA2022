1cff20213e460080ceb4039b40a2a21b899b2afc
Rama Muhammad <rmurshal@gmail.com>
2022-01-11 14:16:47 +0800

    add coming soon

---
+++ app/Views/coming_soon.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr" class="h-100">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ARA 2022</title>
+  <meta name="description" content="Mendorong Inovasi dan Pembangunan Infrastruktur Teknologi untuk Indonesia">
+  <meta name="keywords" content="ARA, A Renewal Agent, Teknologi Informasi, Institut Teknologi Sepuluh Nopember">
+  <meta name="author" content="Divisi Website ARA 2022">
+  <link rel="icon" href="<?= base_url() ?>/images/favicon.ico" type="image/x-icon">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+  <link rel="stylesheet" href="<?= base_url() ?>/css/coming_soon.css">
+</head>
+
+<body class="text-center text-white position-relative">
+
+  <div class="radial-1 position-absolute"></div>
+  <img src="<?= base_url() ?>/images/ring.svg" alt="Ring SVG" class="ring position-absolute">
+
+  <!-- main -->
+  <main class="h-100 d-flex align-items-center animate__animated animate__bounceInDown">
+    <div class="container">
+      <div class="option text-center mb-5">
+        <li><span>KTI IOT</span></li>
+        <li><span>Olimpiade</span></li>
+        <li><span>Capture The Flag</span></li>
+        <li><span>Webinar</span></li>
+        <li><span>Expo IT</span></li>
+      </div>
+      <img src="<?= base_url() ?>/images/rocket.svg" alt="Rocket Icon" width="130" class="mb-5">
+      <h1 class="mb-5">COMING SOON!</h1>
+      <p class="footer">
+        <i class="far fa-copyright"></i>
+        ARA 2022
+      </p>
+    </div>
+  </main>
+
+  <!-- end main -->
+  <div class="radial-2 position-absolute"></div>
+  <img src="<?= base_url() ?>/images/moon.svg" alt="Moon SVG" class="moon position-absolute">
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
