77db357dfdf0f654c3e3d52e70dc6ca654a637eb
Rama Muhammad <rmurshal@gmail.com>
2022-01-26 12:49:05 +0800

    add dashboard kti iot user

--- app/Views/dashboard/user/kti_iot/abstrak.php
+++ app/Views/dashboard/user/kti_iot/abstrak.php
@@ -12,10 +12,10 @@
   <button id="btn-close-side" class="btn shadow-none d-block d-lg-none text-white ms-auto"><i class="fas fa-chevron-left"></i></button>
   <h1 class="fw-bold mb-4 text-white">Dashboard</h1>
   <ul>
-    <li class="active">
+    <li class="">
       <a href="<?= base_url() ?>/dashboard/user_kti_iot/home"><img class="me-2" src="<?= base_url() ?>/images/dashboard/home.svg" alt="Icon"> Home</a>
     </li>
-    <li class="">
+    <li class="active">
       <a href="<?= base_url() ?>/dashboard/user_kti_iot/abstrak"><img class="me-2" src="<?= base_url() ?>/images/dashboard/submission.svg" alt="Icon"> Abstrak</a>
     </li>
     <li class="">
@@ -40,24 +40,38 @@ KTI Internet Of Things
 <h2 class="d-block d-lg-none">KTI Internet Of Things</h2>
 <p class="d-block d-lg-none">Tim IT'03</p>
 <hr class="d-block d-lg-none">
-<h3 class="mb-3">Informasi Umum</h3>
+<h3 class="mb-3">Abstrak</h3>
 
-<!-- guide book -->
+<!-- informasi abstrak -->
 <div class="card-dashboard">
-  <h4>Guide Book</h4>
+  <h4>Informasi Abstrak</h4>
   <ul>
-    <li><i class="fas fa-book"></i> Link: <a href="https://drive.ara.guide.book.com" target="_blank">https://drive.ara.guide.book.com</a></li>
+    <li><i class="fas fa-calendar-alt"></i> Open Registrasi: 27 Februari - 19 Maret</li>
+    <li><i class="fas fa-clock"></i> Deadline Pengumpulan: 22 Maret</li>
   </ul>
 </div>
 
-<!-- data tim -->
+<!-- pengumpulan abstrak -->
 <div class="card-dashboard">
-  <h4>Data Tim</h4>
+  <h4>Pengumpulan Abstrak</h4>
   <ul>
-    <li><i class="fas fa-university"></i> Institusi: Institut Teknologi Sepuluh Nopember</li>
-    <li><i class="fas fa-user"></i> Ketua Tim: Haffif Rasya Fauzi</li>
-    <li><i class="fas fa-user"></i> Anggota 1: Naufal Dhiya Ulhaq</li>
-    <li><i class="fas fa-user"></i> Anggota 2: Fatih Rian Hibatul Hakim</li>
+    <li><i class="fas fa-exclamation-triangle"></i> Pastikan anda sudah menginputkan file yang benar. File hanya bisa diinputkan satu kali.</li>
+    <li>
+      <form action="" method="POST">
+        <div class="mb-4">
+          <input class="form-control" type="file" name="abstrak">
+        </div>
+        <button type="submit" class="btn d-block mx-auto text-white">Submit</button>
+      </form>
+    </li>
+  </ul>
+</div>
+
+<!-- sudah mengumpulkan abstrak -->
+<div class="card-dashboard">
+  <h4>Pengumpulan Abstrak</h4>
+  <ul>
+    <li><i class="fas fa-thumbs-up"></i> Terima kasih sudah mengumpulkan Abstrak. Pantau terus Instagram ARA untuk mengecek hasilnya.</li>
   </ul>
 </div>
 <?= $this->endSection(); ?>
--- app/Views/dashboard/user/kti_iot/belum_full_paper.php
+++ app/Views/dashboard/user/kti_iot/belum_full_paper.php
@@ -12,13 +12,13 @@
   <button id="btn-close-side" class="btn shadow-none d-block d-lg-none text-white ms-auto"><i class="fas fa-chevron-left"></i></button>
   <h1 class="fw-bold mb-4 text-white">Dashboard</h1>
   <ul>
-    <li class="active">
+    <li class="">
       <a href="<?= base_url() ?>/dashboard/user_kti_iot/home"><img class="me-2" src="<?= base_url() ?>/images/dashboard/home.svg" alt="Icon"> Home</a>
     </li>
     <li class="">
       <a href="<?= base_url() ?>/dashboard/user_kti_iot/abstrak"><img class="me-2" src="<?= base_url() ?>/images/dashboard/submission.svg" alt="Icon"> Abstrak</a>
     </li>
-    <li class="">
+    <li class="active">
       <a href="<?= base_url() ?>/dashboard/user_kti_iot/full_paper"><img class="me-2" src="<?= base_url() ?>/images/dashboard/submission.svg" alt="Icon"> Full Paper</a>
     </li>
     <li class="">
@@ -40,25 +40,17 @@ KTI Internet Of Things
 <h2 class="d-block d-lg-none">KTI Internet Of Things</h2>
 <p class="d-block d-lg-none">Tim IT'03</p>
 <hr class="d-block d-lg-none">
-<h3 class="mb-3">Informasi Umum</h3>
+<h3 class="mb-3">Full Paper</h3>
 
-<!-- guide book -->
-<div class="card-dashboard">
-  <h4>Guide Book</h4>
-  <ul>
-    <li><i class="fas fa-book"></i> Link: <a href="https://drive.ara.guide.book.com" target="_blank">https://drive.ara.guide.book.com</a></li>
-  </ul>
-</div>
-
-<!-- data tim -->
-<div class="card-dashboard">
-  <h4>Data Tim</h4>
-  <ul>
-    <li><i class="fas fa-university"></i> Institusi: Institut Teknologi Sepuluh Nopember</li>
-    <li><i class="fas fa-user"></i> Ketua Tim: Haffif Rasya Fauzi</li>
-    <li><i class="fas fa-user"></i> Anggota 1: Naufal Dhiya Ulhaq</li>
-    <li><i class="fas fa-user"></i> Anggota 2: Fatih Rian Hibatul Hakim</li>
-  </ul>
+<!-- belum lulus -->
+<div class="mx-auto" id="container-belum-lulus">
+  <div class="p-3" id="container-img-belum-lulus">
+    <img src="<?= base_url() ?>/images/dashboard/belum_lulus.svg" alt="Icon" class="img-fluid d-block mx-auto">
+  </div>
+  <div class="text-center p-3" id="container-text-belum-lulus">
+    <h3>Mohon Maaf</h3>
+    <p>Tim anda belum dapat lanjut ke tahap ini</p>
+  </div>
 </div>
 <?= $this->endSection(); ?>
 <!-- /end content -->
