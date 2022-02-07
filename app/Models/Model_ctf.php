fcbed6fbc8980dd356e90958fc4968c8af04df80
Amrega <rtpamrega@gmail.com>
2022-02-07 23:01:20 +0700

    done adding function for regis ctf, webinar, expo and add some column to db

---
+++ app/Models/Model_ctf.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Model_ctf extends Model
+{
+    // ...
+    protected $table = 'ctf';
+    protected $primaryKey = 'ctf_id';
+    protected $allowedFields = [
+        'ctf_nama_tim', 
+        'ctf_jumlah_anggota', 
+        'ctf_email_ketua ', 
+        'ctf_nama_ketua', 
+        'ctf_nama_anggota_1', 
+        'ctf_nama_anggota_2', 
+        'ctf_suket_ketua', 
+        'ctf_suket_anggota_1', 
+        'ctf_suket_anggota_2', 
+        'ctf_ig_ara_ketua',
+        'ctf_ig_ara_anggota_1',
+        'ctf_ig_ara_anggota_2', 
+        'ctf_ig_hmit_ketua ', 
+        'ctf_ig_hmit_anggota_1 ', 
+        'ctf_ig_hmit_anggota_2', 
+        'ctf_intitusi', 
+        'ctf_contact', 
+        'ctf_status_final',
+        'ctf_bukti_bayar', 
+        'ctf_status'
+    ];
+    protected $useTimestamps = true;
+    protected $createdField = 'ctf_date_created';
+    protected $updatedField = 'ctf_date_updated';
+}
