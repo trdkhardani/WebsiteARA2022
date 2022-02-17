dda5c0817379147239871aca6c35dd793dddd0ce
martuafernando <fernandosibarani45@gmail.com>
2022-02-17 22:50:44 +0700

    add image dokumentasi

--- app/Views/landing_page/component/HMIT-card.php
+++ app/Views/landing_page/component/HMIT-card.php
@@ -3,22 +3,46 @@
     <h2 class="h1 text-center font-weight-bold mt-4">HMIT ITS</h2>
     <p class="text-center my-1 py-3 w-75 mx-auto">HMIT merupakan himpunan yang mengayomi mahasiswa departmen Teknologi Informasi ITS. Didirikan pada November 2019 dan sejak saat itu telah mengadakan 2 event besar yakni ExploIT 2020 dan ARA 2021 yang sukses dan diikuti oleh ribuan peserta.</p>
     <div class="mx-auto text-center d-flex justify-content-center mb-5">
-        <a class="sosial-media mx-2 rounded-circle" href="https://www.instagram.com/hmit_its/" rel="nofollow" target="_blank"><i class="fab fa-instagram"></i></a>
-        <a class="sosial-media mx-2 rounded-circle" href="https://timeline.line.me/user/_dY3IhpWxTrFRR2jUUi8zD00dbhy0yqgnmZppCC8" target="_blank" rel="nofollow"><i class="fab fa-line"></i></a>
-        <a class="sosial-media mx-2 rounded-circle" href="https://www.linkedin.com/company/himpunan-mahasiswa-teknologi-informasi-its" rel="nofollow" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-        <a class="sosial-media mx-2 rounded-circle" href="https://www.youtube.com/channel/UChWjgJMQIYVivMx_gIy1eDA" rel="nofollow" target="_blank"><i class="fab fa-youtube"></i></a>
-        <a class="sosial-media mx-2 rounded-circle" href="https://www.its.ac.id/it/" rel="dofollow" target="_blank"><i class="fas fa-globe"></i></a>
+        <a class="sosial-media mx-2 rounded-circle" target="_blank" href="https://www.instagram.com/hmit_its/" rel="nofollow" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a class="sosial-media mx-2 rounded-circle" target="_blank" href="https://timeline.line.me/user/_dY3IhpWxTrFRR2jUUi8zD00dbhy0yqgnmZppCC8" target="_blank" rel="nofollow"><i class="fab fa-line"></i></a>
+        <a class="sosial-media mx-2 rounded-circle" target="_blank" href="https://www.linkedin.com/company/himpunan-mahasiswa-teknologi-informasi-its" rel="nofollow" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+        <a class="sosial-media mx-2 rounded-circle" target="_blank" href="https://www.youtube.com/channel/UChWjgJMQIYVivMx_gIy1eDA" rel="nofollow" target="_blank"><i class="fab fa-youtube"></i></a>
+        <a class="sosial-media mx-2 rounded-circle" target="_blank" href="https://www.its.ac.id/it/" rel="dofollow" target="_blank"><i class="fas fa-globe"></i></a>
     </div>
     <div id="carousel-HMIT" class="w-50 mx-auto carousel slide pb-5" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="<?= base_url() ?>/images/example.png" class="d-block w-100" alt="...">
+                <img src="<?= base_url() ?>/images/dokumentasi2021/(1).jpg" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="<?= base_url() ?>/images/example.png" class="d-block w-100" alt="...">
+                <img src="<?= base_url() ?>/images/dokumentasi2021/(2).jpg" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="<?= base_url() ?>/images/example.png" class="d-block w-100" alt="...">
+                <img src="<?= base_url() ?>/images/dokumentasi2021/(3).jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= base_url() ?>/images/dokumentasi2021/(4).jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= base_url() ?>/images/dokumentasi2021/(5).jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= base_url() ?>/images/dokumentasi2021/(6).jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= base_url() ?>/images/dokumentasi2021/(7).jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= base_url() ?>/images/dokumentasi2021/(8).jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= base_url() ?>/images/dokumentasi2021/(9).jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= base_url() ?>/images/dokumentasi2021/(10).jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="<?= base_url() ?>/images/dokumentasi2021/(11).jpg" class="d-block w-100" alt="...">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carousel-HMIT" data-bs-slide="prev">
