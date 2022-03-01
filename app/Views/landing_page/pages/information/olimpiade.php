9427517ce1449b86518bedd81aee05ae42bb1fec
Rama Muhammad Murshal <81663790+ramammurshal@users.noreply.github.com>
2022-03-01 23:39:55 +0700

    Change guide book olim at view details

--- app/Views/landing_page/pages/information/olimpiade.php
+++ app/Views/landing_page/pages/information/olimpiade.php
@@ -9,7 +9,7 @@
     <div class="col-md-6">
         <h1>Olimpiade</h1>
         <p class="mt-4">Olimpiade ARA 2022 adalah olimpiade nasional dengan topik Cyber Security (keamanan siber) dan IoT berbasis tim yang terdiri dari 1-3 siswa/siswi SMA/SMK yang akan berkompetisi dalam 2 babak, yaitu penyisihan dengan soal teori dan final mengenai studi kasus beserta solusinya. Tunjukkan pengetahuan dan taring kalian dalam kompetisi Olimpiade ARA 2022!</p>
-        <a href="https://drive.google.com/file/d/16FGqHRsCI-iI1bZxJ5Lvlemk47hJhoAP/view?usp=sharing" target="_blank" class="btn btn-primary px-4 py-3 mt-4 me-4 rounded-pill">Download Guide Book</a>
+        <a href="https://drive.google.com/file/d/1YM3RAGTAefTf8x_hTTfd3XZGyjvZG9oc/view?usp=drivesdk" target="_blank" class="btn btn-primary px-4 py-3 mt-4 me-4 rounded-pill">Download Guide Book</a>
         <a href="<?= base_url() ?>/auth/registrasi_olim" class="btn btn-primary px-4 py-3 mt-4 rounded-pill">Daftar Olimpiade</a>
     </div>
     <div class="col-md-6">
