79793d287830943a08194b776f5d3c2c0082c650
martuafernando <fernandosibarani45@gmail.com>
2022-03-02 09:08:13 +0700

    fixing bug

--- app/Views/landing_page/pages/information/webinar.php
+++ app/Views/landing_page/pages/information/webinar.php
@@ -9,11 +9,10 @@
     <div class="col-md-6">
         <h1>Webinar</h1>
         <p class="mt-4">Webinar merupakan acara pertama dalam rangkaian kegiatan ARA 2022 yang akan membahas dua topik khusus yaitu Internet of Things dan Cyber Security dimana pembicaranya ialah orang terkemuka dari sektor industri terkait. Pengetahuan adalah sesuatu yang paling berharga di dunia ini. Oleh karena itu, ikuti kegiatan webinar ARA 2022 untuk memperluas pengetahuan kalian!</p>
-        <a href="#" class="btn btn-primary px-4 py-3 mt-4 me-4 rounded-pill disabled">Download Guide Book</a>
         <a href="<?= base_url() ?>/auth/registrasi_webinar" class="btn btn-primary px-4 py-3 mt-4 rounded-pill">Daftar Webinar</a>
     </div>
     <div class="col-md-6">
-        <img src="<?= base_url() ?>/images/manusia-laptop.svg" alt="" class="ms-auto">
+        <img src="<?= base_url() ?>/images/manusia-laptop.svg" alt="" class="ms-auto mt-4">
     </div>
 </section>
 <section>
@@ -37,7 +36,7 @@
             </div>
         </div>
         <div class="event kanan mt-5">
-            <div data-aos="fade-right" class="isi-event">
+            <div data-aos="fade-left" class="isi-event">
                 <p class="fw-bold">IOT</p>
                 <p>10 April 2022</p>
             </div>
