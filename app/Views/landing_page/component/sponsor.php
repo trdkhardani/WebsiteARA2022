011f642d950b047e479ca6d822859809efc496bb
martuafernando <fernandosibarani45@gmail.com>
2022-04-10 23:58:04 +0700

    add sponsor and media partner image

--- app/Views/landing_page/component/sponsor.php
+++ app/Views/landing_page/component/sponsor.php
@@ -1,7 +1,60 @@
 <section data-aos-duration="1500" data-aos="fade-up" class="container">
-    <small class="h3 mx-auto mb-5 d-block text-center fw-bold">Sponsor dan Support</small>
-    <div class="row justify-content-center mx-auto" style="max-width: 768px">
-        <img class="col-4" src="<?= base_url() ?>/images/HMIT 1.png" alt="">
-        <img class="col-4" src="<?= base_url() ?>/images/logo-its.png" alt="">
+    <small class="h3 mx-auto mb-5 d-block text-center fw-bold">Sponsor</small>
+    <div class="row justify-content-center mx-auto">
+        <div class=" col-4 my-auto">
+            <img class="px-3 py-4" src="<?= base_url() ?>/images/sponsor/L_logo Paragon Crop - 16.png" alt="">
+        </div>
+        <div class="col-2 my-auto">
+            <img class="px-3 py-4" src="<?= base_url() ?>/images/sponsor/S_antares.png" alt="">
+        </div>
+        <div class="col-2 my-auto">
+            <img class="px-3 py-4" src="<?= base_url() ?>/images/sponsor/S_indigospace-sub-logo-red.png" alt="">
+        </div>
+        <div class="col-6 my-auto">
+            <img class="px-3 py-4" src="<?= base_url() ?>/images/sponsor/XL_Logo Kencana Ahlinya Baja Ringan.png" alt="">
+        </div>
+    </div>
+
+    <small class="h3 mx-auto mt-5 py-5 d-block text-center fw-bold">Media Partner</small>
+    <div class="row gap-2 justify-content-center mx-auto">
+        <div class="col-2 my-auto">
+            <img class="p-4" src="<?= base_url() ?>/images/Logo Media Partner/Event Jawa Timur.png" alt="">
+        </div>
+        <div class="col-2 my-auto">
+            <img class="p-4" src="<?= base_url() ?>/images/Logo Media Partner/Logo @eventsurabaya.png" alt="">
+        </div>
+        <div class="col-2 my-auto">
+            <img class="p-4" src="<?= base_url() ?>/images/Logo Media Partner/LOGO INFO OLIMPIADE.png" alt="">
+        </div>
+        <div class="col-2 my-auto">
+            <img class="p-4" src="<?= base_url() ?>/images/Logo Media Partner/Logo @Info Event.png" alt="">
+        </div>
+        <div class="col-2 my-auto">
+            <img class="p-4" src="<?= base_url() ?>/images/Logo Media Partner/logo-himsi-46.png" alt="">
+        </div>
+        <div class=" col-2 my-auto">
+            <img class="p-4" src="<?= base_url() ?>/images/Logo Media Partner/1646459209049.jpg" alt="">
+        </div>
+        <div class="col-2 my-auto">
+            <img class="p-4" src="<?= base_url() ?>/images/Logo Media Partner/himtiug-logo.png" alt="">
+        </div>
+        <div class="col-2 my-auto">
+            <img class="p-4" src="<?= base_url() ?>/images/Logo Media Partner/Logo @acaramahasiswa .jpeg" alt="">
+        </div>
+        <div class="col-2 my-auto">
+            <img class="p-4" src="<?= base_url() ?>/images/Logo Media Partner/LOGO @eventcampus.png" alt="">
+        </div>
+        <div class="col-2 my-auto">
+            <img class="p-4" src="<?= base_url() ?>/images/Logo Media Partner/Logo @infolomba.png" alt="">
+        </div>
+        <div class="col-2 my-auto">
+            <img class="p-4" src="<?= base_url() ?>/images/Logo Media Partner/Logo @infoselanjutnya.png" alt="">
+        </div>
+        <div class="col-2 my-auto">
+            <img class="p-4" src="<?= base_url() ?>/images/Logo Media Partner/LOGO HIMATIF 2.0.png" alt="">
+        </div>
+        <div class="col-2 my-auto">
+            <img class="p-4" src="<?= base_url() ?>/images/Logo Media Partner/LOGO SOAL EVENT.jpg" alt="">
+        </div>
     </div>
 </section>
