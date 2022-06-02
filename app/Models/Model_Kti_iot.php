4573687ce95cbe4dd4f89e896c1a56925d34d7cf
Tridiktya Putra <tridiktya@gmail.com>
2022-06-03 05:45:08 +0700

    model controller juara added

--- app/Models/Model_Kti_iot.php
+++ app/Models/Model_Kti_iot.php
@@ -8,7 +8,7 @@ class Model_Kti_iot extends Model
 {
     protected $table = 'kti_iot';
     protected $primaryKey = 'iot_id';
-    protected $allowedFields = ['iot_nama_tim', 'iot_jumlah_anggota', 'iot_email_ketua', 'iot_nama_ketua', 'iot_nama_anggota_1', 'iot_nama_anggota_2', 'iot_suket_ketua', 'iot_suket_anggota_1', 'iot_suket_anggota_2', 'iot_krsm_ketua', 'iot_krsm_anggota_1', 'iot_krsm_anggota_2', 'iot_contact', 'iot_institusi', 'iot_story_ketua', 'iot_story_anggota_1', 'iot_story_anggota_2', 'iot_ig_ara_ketua', 'iot_ig_ara_anggota_1', 'iot_ig_ara_anggota_2', 'iot_ig_hmit_ketua', 'iot_ig_hmit_anggota_1', 'iot_ig_hmit_anggota_2', 'iot_abstrak', 'iot_kti_paper', 'iot_status_penyisihan', 'iot_status_final', 'iot_pembayaran_full_paper', 'iot_pembayaran_final', 'iot_status_konfirmasi_abstrak', 'iot_status_konfirmasi_full_paper', 'iot_status_konfirmasi_final'];
+    protected $allowedFields = ['iot_nama_tim', 'iot_jumlah_anggota', 'iot_email_ketua', 'iot_nama_ketua', 'iot_nama_anggota_1', 'iot_nama_anggota_2', 'iot_suket_ketua', 'iot_suket_anggota_1', 'iot_suket_anggota_2', 'iot_krsm_ketua', 'iot_krsm_anggota_1', 'iot_krsm_anggota_2', 'iot_contact', 'iot_institusi', 'iot_story_ketua', 'iot_story_anggota_1', 'iot_story_anggota_2', 'iot_ig_ara_ketua', 'iot_ig_ara_anggota_1', 'iot_ig_ara_anggota_2', 'iot_ig_hmit_ketua', 'iot_ig_hmit_anggota_1', 'iot_ig_hmit_anggota_2', 'iot_abstrak', 'iot_kti_paper', 'iot_status_penyisihan', 'iot_status_final', 'iot_juara', 'iot_pembayaran_full_paper', 'iot_pembayaran_final', 'iot_status_konfirmasi_abstrak', 'iot_status_konfirmasi_full_paper', 'iot_status_konfirmasi_final'];
     protected $useTimestamps = true;
     protected $nama_tim = 'iot_nama_tim';
     protected $nama_ketua = 'iot_nama_ketua';
--- app/Models/Model_Olimpiade.php
+++ app/Models/Model_Olimpiade.php
@@ -9,7 +9,7 @@ class Model_Olimpiade extends Model
     // ...
     protected $table = 'olimpiade';
     protected $primaryKey = 'olim_id';
-    protected $allowedFields = ['olim_nama_tim', 'olim_jumlah_anggota', 'olim_email_ketua', 'olim_nama_ketua', 'olim_nama_anggota_1', 'olim_nama_anggota_2', 'olim_suket_ketua', 'olim_suket_anggota_1', 'olim_suket_anggota_2', 'olim_contact', 'olim_ig_ara_ketua', 'olim_ig_ara_anggota_1', 'olim_ig_ara_anggota_2', 'olim_ig_hmit_ketua', 'olim_ig_hmit_anggota_1', 'olim_ig_hmit_anggota_2', 'olim_institusi', 'olim_status', 'olim_status_final', 'olim_pembayaran'];
+    protected $allowedFields = ['olim_nama_tim', 'olim_jumlah_anggota', 'olim_email_ketua', 'olim_nama_ketua', 'olim_nama_anggota_1', 'olim_nama_anggota_2', 'olim_suket_ketua', 'olim_suket_anggota_1', 'olim_suket_anggota_2', 'olim_contact', 'olim_ig_ara_ketua', 'olim_ig_ara_anggota_1', 'olim_ig_ara_anggota_2', 'olim_ig_hmit_ketua', 'olim_ig_hmit_anggota_1', 'olim_ig_hmit_anggota_2', 'olim_institusi', 'olim_status', 'olim_juara', 'olim_status_final', 'olim_pembayaran'];
     protected $useTimestamps = true;
     protected $createdField = 'olim_date_created';
     protected $updatedField = 'olim_date_updated';
